7033e7dcac6408b889775fe4b72cf87825b08fb9
Mike <mh@andsoluciones.com>
2026-05-02 12:18:27 -0500

    Make tmuxinator template configurable in gw-tmuxinator wrapper
    Add GW_TMUXINATOR_TEMPLATE environment variable support.
    Defaults to 'worktree-ai' if not set.

--- wrappers/gw-tmuxinator.fish
+++ wrappers/gw-tmuxinator.fish
@@ -1,10 +1,12 @@
 function gw-tmuxinator --description "Open worktree in a tmuxinator session"
     set -l wt $argv[1]
     set -l session (basename $wt)
+    set -l template "worktree-ai"
+    set -q GW_TMUXINATOR_TEMPLATE; and set template $GW_TMUXINATOR_TEMPLATE
 
     if tmux has-session -t $session 2>/dev/null
         tmux attach -t $session
     else
-        tmuxinator start worktree-ai $session $wt
+        tmuxinator start $template $session $wt
     end
 end
