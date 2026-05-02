c93367a5d5cb0bb8d5841e2e2bd6faf8fcfd36d1
Mike <mh@andsoluciones.com>
2026-05-02 12:42:19 -0500

    Change default tmuxinator template to 'worktree'

--- wrappers/gw-tmuxinator.fish
+++ wrappers/gw-tmuxinator.fish
@@ -1,7 +1,7 @@
 function gw-tmuxinator --description "Open worktree in a tmuxinator session"
     set -l wt $argv[1]
     set -l session (basename $wt)
-    set -l template "worktree-ai"
+    set -l template "worktree"
     set -q GW_TMUXINATOR_TEMPLATE; and set template $GW_TMUXINATOR_TEMPLATE
 
     if tmux has-session -t $session 2>/dev/null
